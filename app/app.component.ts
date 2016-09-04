88ccbc507df807992060524efb938886c53054f5
chsakell <chsakell@gmail.com>
2016-09-04 22:17:06 +0300

    add getheroes slowly method

--- app/app.component.ts
+++ app/app.component.ts
@@ -84,7 +84,7 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
 
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,4 +8,11 @@ export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000)) // delay 2 seconds
+            .then(() => this.getHeroes());
+    }
+
 }
