1c4ca376773be89ac455c6081813afee7e4e6981
chsakell <chsakell@gmail.com>
2016-09-04 21:47:45 +0300

    separate hero-details from app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
+
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
     { id: 12, name: 'Narco' },
@@ -28,12 +26,7 @@ const HEROES: Hero[] = [
       </li>
     </ul>
     <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     </div>
   `,
     styles: [`
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }   from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule],
-    declarations: [AppComponent],
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
